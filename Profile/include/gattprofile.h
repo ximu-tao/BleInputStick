795f3cb8602f8a4b70111d068c73122342d348c5
ximu-tao <me_litao@outlook.com>
2023-10-20 09:18:26 +0800

    增加蓝牙缓冲器大小
    Signed-off-by: ximu-tao <me_litao@outlook.com>

--- Profile/include/gattprofile.h
+++ Profile/include/gattprofile.h
@@ -46,11 +46,11 @@ extern "C" {
 #define SIMPLEPROFILE_SERVICE       0x00000001
 
 // Length of characteristic in bytes ( Default MTU is 23 )
-#define SIMPLEPROFILE_CHAR1_LEN     1
-#define SIMPLEPROFILE_CHAR2_LEN     1
-#define SIMPLEPROFILE_CHAR3_LEN     1
-#define SIMPLEPROFILE_CHAR4_LEN     1
-#define SIMPLEPROFILE_CHAR5_LEN     5
+#define SIMPLEPROFILE_CHAR1_LEN     19
+#define SIMPLEPROFILE_CHAR2_LEN     19
+#define SIMPLEPROFILE_CHAR3_LEN     19
+#define SIMPLEPROFILE_CHAR4_LEN     19
+#define SIMPLEPROFILE_CHAR5_LEN     19
 
 /*********************************************************************
  * TYPEDEFS
